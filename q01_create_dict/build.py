5147e6ffd97aed5a0969f70adc743e258845dba2
greyatomtestuser2 <greyatomtestuser2@gmail.com>
2018-03-24 12:10:35 +0000

    Done

--- q01_create_dict/build.py
+++ q01_create_dict/build.py
@@ -1,8 +1,10 @@
+# %load q01_create_dict/build.py
 import pandas as pd
+from collections import Counter
 
 path = 'data/babies_name.csv'
 data = pd.read_csv(path,names=['Name', 'Gender', 'Count', 'Year'])
 
-def q01_create_dict():
-    "write your solution here"
-
+def q01_create_dict(data):
+    a = dict(zip(data.Name, data.Count))
+    return a
