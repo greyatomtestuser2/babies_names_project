5c78e69fc9ef66932c5d7517b88d4c75d9803f43
greyatomtestuser2 <greyatomtestuser2@gmail.com>
2018-03-27 17:26:53 +0000

    Done

--- q04_top_in_each_year_2/build.py
+++ q04_top_in_each_year_2/build.py
@@ -1,3 +1,4 @@
+# %load q04_top_in_each_year_2/build.py
 import pandas as pd
 import numpy as np
 from greyatomlib.babies_names_project.q03_top_in_each_year_1.build import q03_top_in_each_year_1
@@ -6,6 +7,9 @@ from collections import Counter
 path = 'data/babies_name.csv'
 data = pd.read_csv(path,names=['Name', 'Gender', 'Count', 'Year'])
 
-def q04_top_in_each_year_2():
-    "write your solution here"
+def q04_top_in_each_year_2(data):
+    all_sum = []
+    a = Counter(data['Name']).most_common(25)
+    all_sum.append(sum(Counter(a).values()))
 
+    return all_sum
