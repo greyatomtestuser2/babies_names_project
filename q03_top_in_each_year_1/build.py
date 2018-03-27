28f9ba9a03329ae4ebba5a71bee2e6797c3a0186
greyatomtestuser2 <greyatomtestuser2@gmail.com>
2018-03-27 15:52:24 +0000

    Done

--- q03_top_in_each_year_1/build.py
+++ q03_top_in_each_year_1/build.py
@@ -1,9 +1,12 @@
-
+# %load q03_top_in_each_year_1/build.py
+import numpy as np
 import pandas as pd
+from collections import Counter
 
 path = 'data/babies_name.csv'
 data = pd.read_csv(path,names=['Name', 'Gender', 'Count', 'Year'])
 
-def q03_top_in_each_year_1():
-    "write your solution here"
-
+def q03_top_in_each_year_1(data):
+    c = np.unique(data['Name'])
+    a_dict = dict(zip(c, data.Count))
+    return a_dict
